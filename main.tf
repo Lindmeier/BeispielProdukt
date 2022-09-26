a09b40613c155c84bbac0c0e89e6dc3e9ef7c7a5
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-09-26 15:35:52 +0200

    add server

--- main.tf
+++ main.tf
@@ -1,9 +1,38 @@
-module "generator" {
+# Provider Configuration 
+provider "fakewebservices" {
+  token = var.provider_token
+}
+
+# Module Calls (Instanziierung der Bausteine)
+module "NAME_VM1_3e53191b-e96d-4e8b-ba66-89bdafbb9dee" {
   source  = "app.terraform.io/team-automation/generator/random"
   version = "1.0.0"
   type = "VM"
 }
 
-output "name" {
-  value = module.generator.official_name
+module "NAME_VM2_3e53191b-e96d-4e8b-ba66-89bdafbb9dee" {
+  source  = "app.terraform.io/team-automation/generator/random"
+  version = "1.0.0"
+  type = "VM"
+}
+
+module "VM1_3e53191b-e96d-4e8b-ba66-89bdafbb9dee" {
+  source  = "app.terraform.io/team-automation/VM/fakewebservices"
+  version = "1.0.0"
+  # insert required variables here
+  official_name = module.NAME_VM1_3e53191b-e96d-4e8b-ba66-89bdafbb9dee.official_name
+  type = "webserver"
+}
+
+module "VM2_3e53191b-e96d-4e8b-ba66-89bdafbb9dee" {
+  source  = "app.terraform.io/team-automation/VM/fakewebservices"
+  version = "1.0.0"
+  # insert required variables here
+  official_name = module.NAME_VM2_3e53191b-e96d-4e8b-ba66-89bdafbb9dee.official_name
+  type = "webserver"
+}
+
+# Output
+output "VM_LIST" {
+  value = tolist([module.VM1_3e53191b-e96d-4e8b-ba66-89bdafbb9dee.vm_id, module.VM2_3e53191b-e96d-4e8b-ba66-89bdafbb9dee.vm_id ])
 }
